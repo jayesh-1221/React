a188374d681e254bb035a2e220e162c901e4113d
jayesh-1221 <jayeshjoshi1208@gmail.com>
2025-07-14 14:50:53 +0530

    ToDo Application using Context API

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,33 +1,34 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { ThemeProvider } from "./context/Theme";
+import { ThemeProvider as ThemeContextProvider } from "./context/Theme";
 import ThemeBtn from "./components/ThemeButton";
 import Card from "./components/card";
 
+export const ThemeProvider = ThemeContextProvider();
+
 function App() {
-  const [themeMode,setThemeMode]=useState('light');
+  const [themeMode, setThemeMode] = useState("light");
 
-  const darktheme=()=>{
-    setThemeMode('dark');
-  }
+  const darktheme = () => {
+    setThemeMode("dark");
+  };
 
-  const lighttheme=()=>{
-    setThemeMode('light');
-  }
+  const lighttheme = () => {
+    setThemeMode("light");
+  };
 
-  useEffect(()=>{
-        document.querySelector('html').classList.remove('light','dark');
-        document.querySelector('html').classList.add(themeMode);
-  },[themeMode]);
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <ThemeProvider value={{themeMode,light}}>
+    <ThemeProvider value={{ themeMode, darktheme, lighttheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
              <ThemeBtn/>
           </div>
-            
           <div className="w-full max-w-sm mx-auto">
             <Card/>
           </div>
